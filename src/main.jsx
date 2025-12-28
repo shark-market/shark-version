84e37cc26efc4d9f9eb6205a2249c91db87498fd
mo <mo@mos-MacBook-Pro.local>
2025-12-28 15:42:32 +0300

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
@@ -8,12 +8,12 @@ import { CurrencyProvider } from "./context/CurrencyContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CurrencyProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </CurrencyProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
